61e3bde361b82c042deaf4e3d555ea0295c6f249
Chris <christoph.tittel@gmail.com>
2020-08-08 23:22:15 +0200

    fix config for vim

--- install.py
+++ install.py
@@ -1,17 +1,17 @@
 import os
 import platform
-from pathlib import Path
+from pathlib import Path, PosixPath
 from vimfiles.setup import vim_setup
 
 is_windows = (platform.system() == 'Windows')
 is_linux = (platform.system() == "Linux")
 
-def create_symlink(src, dest, is_dir=False, newname=None):
-    """ Returns True if symlink is created, False if it already exists """
+def create_symlink(src, dest, is_dir=False):
+    """ Returns True if symlink is created, False if it already exists.
+    src: existing file
+    dest: doesnt exist yet, place where symlink is created """
 
-    if not newname:
-        newname = src.name
-    dest = dest / newname
+    print("Creating symlink with src: ", src, " dest: ", dest)
 
     if dest.exists():
         print(dest, "exists already, updating")
@@ -21,20 +21,24 @@ def create_symlink(src, dest, is_dir=False, newname=None):
     else:
         os.symlink(src, dest, is_dir)
         return True
-    
 
 local = Path(__file__).parent.absolute()
-user = Path.home()
+if is_windows:  
+    user = Path.home()
+elif is_linux:
+    user = PosixPath('~')
+    user = user.expanduser()
+
+print("local: ", local)
+print("user: ", user)
 
 if is_windows:
     print("Setting up symlinks for Windows")
-    create_symlink(local / ".emacs.d", user / "AppData" / "Roaming", is_dir=True)
-    if create_symlink(local / "vimfiles", user, is_dir = True):
-        print("Run installation script for vim")
+    create_symlink(local / ".emacs.d", user / "AppData" / "Roaming" / ".emacs.d", is_dir=True)
+    create_symlink(local / "vimfiles", user / "vimfiles", is_dir = True)
     vim_setup()
 elif is_linux:
     print("Setting up symlinks for Linux")
-    os.symlink(local / ".emacs.d", user, True)
-    if os.symlink(local / "vimfiles", user, True, newname='.vim'):
-        print("Run installation script for vim")
+    create_symlink(local / ".emacs.d", user / ".emacs.d", True)
+    create_symlink(local / "vimfiles", user / ".vim", True)
     vim_setup()
